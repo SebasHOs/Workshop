0e03cedd94b543000b6c0a8ba041a2a0a1e28a31
SebasHOs <133323576+SebasHOs@users.noreply.github.com>
2026-03-20 21:55:59 -0600

    added

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res, next) => {
-    res.status(200).send("Hey muy buenas a todos");
+    res.status(200).send("Hey muy buenas a todos!");
 });
 
-app.listen(3000, () => {
-    console.log("Server up and running.");
+app.get("/:nombre", (req, res, next) =>{
+    console.log(req.params.name);
+    res.status(200);
+    res.send("Estas en la pagina 'nombre'");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server up and running...");
 });
