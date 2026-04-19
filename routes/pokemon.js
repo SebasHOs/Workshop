91f33359e6573676b16ab79af9a260dcf27cf96a
SebasHOs <133323576+SebasHOs@users.noreply.github.com>
2026-04-19 15:19:59 -0600

    metodos patch y put

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -4,6 +4,7 @@ const db = require('../config/database');
 
 pokemon.post("/", async (req, res, next) => {
     const { pok_name, pok_height, pok_weight, pok_base_experience } = req.body;
+
     if(pok_name && pok_height && pok_weight && pok_base_experience) {
     let query = "INSERT INTO pokemon (pok_name, pok_height, pok_weight, pok_base_experience)";
     query +=  `VALUES('${pok_name}', ${pok_height}, ${pok_weight}, ${pok_base_experience})`;
@@ -15,6 +16,49 @@ return res.status(201).json({ code: 201, message: "Pokemon creado exitosamente"}
  return res.status(500).json({ code: 500, message: "Error al crear el Pokemon" });
     }
 return res.status(500),express.json({ code: 500, message: "Faltan datos para crear el Pokemon" });
+});
+
+pokemon.delete(/^\/\/([0-9]{1,3})$/, async (req, res, next) => {
+const query = `DELETE FROM pokemon WHERE pok_id = ${req.params.id}`;
+
+const rows = await db.query(query);
+
+if (rows.affectedRows == 1) {
+    return res.status(200).json({ code: 200, message: "Pokemon eliminado exitosamente" });
+}
+return res.status(404).json({ code: 404, message: "Pokemon no encontrado" });
+
+});
+
+pokemon.put(/^\/\/([0-9]{1,3})$/, async (req, res, next) => {
+const { pok_name, pok_height, pok_weight, pok_base_experience } = req.body;
+
+     if(pok_name && pok_height && pok_weight && pok_base_experience) {
+    let query = `UPDATE pokemon SET pok_name='${pok_name}',pok_height=${pok_height}, `;
+    query += `pok_weight=${pok_weight},pok_base_experience=${pok_base_experience} WHERE pok_id=${req.params.id}`;
+    const rows = await db.query(query); 
+    
+    if(rows.affectedRows == 1) {
+return res.status(200).json({ code: 200, message: "Pokemon actualizado exitosamente"});
+    }
+ return res.status(500).json({ code: 500, message: "Error al crear el Pokemon" });
+    }
+return res.status(500),express.json({ code: 500, message: "Faltan datos para crear el Pokemon" });
+
+});
+
+pokemon.patch(/^\/\/([0-9]{1,3})$/, async (req, res, next) => {
+if(req.body.pok_name) {
+    let query = `UPDATE pokemon SET pok_name='${req.body.pok_name}',WHERE pok_id=${req.params.id}`;    
+    const rows = await db.query(query); 
+    
+    if(rows.affectedRows == 1) {
+return res.status(200).json({ code: 200, message: "Pokemon actualizado exitosamente"});
+    }
+    
+return res.status(500).json({ code: 500, message: "Error al actualizar el Pokemon" });
+}
+return res.status(500),express.json({ code: 500, message: "Faltan datos para actualizar el Pokemon" });
 });
 
 pokemon.get('/',async (req, res, next) => {
