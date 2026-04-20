5d0f14ad9f7d02448574fcfbcba4e88ac29d445b
SebasHOs <133323576+SebasHOs@users.noreply.github.com>
2026-04-19 21:40:30 -0600

    added login

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const user = express.Router();
+const jwt = require('jsonwebtoken');
 const db = require('../config/database');
 
 user.post("/", async (req, res, next) => {
@@ -15,5 +16,33 @@ return res.status(500).json({ code: 500, message: "Error al crear el Usuario" })
 
 return res.status(500).json({ code: 500, message: "Campos incompletos" });
 });
+
+user.post("/login", async (req, res, next) => {
+
+    const { user_mail, user_password } = req.body;
+    const query = "SELECT * FROM user WHERE user_mail = '${user_mail}'  AND user_password = '${user_password}'";
+    const rows = await db.query(query);
+
+    if (user_mail && user_password) {
+        if (rows.length == 1) {
+            const token = jwt.sign({
+                user_id: rows[0].user_id,
+                user_mail: rows[0].user_mail,
+            }, "debugkey");
+            return res.status(200).json({ code: 200, message: token});
+        } 
+        else {        
+        return res.status(401).json({ code: 401, message: "Credenciales incorrectas" });
+    }
+}
+    return res.status(500).json({ code: 500, message: "Campos incompletos" });
+});
+
+user.get("/", async (req, res, next) => {
+    const query = "SELECT * FROM user";
+    const rows = await db.query(query);
+    return res.status(200).json({ code: 200, message: "Usuarios obtenidos exitosamente", data: rows });
+}); 
+
 
 module.exports = user;
