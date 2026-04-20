a1cbd48586ef93df85059b346204e20d294723d9
SebasHOs <133323576+SebasHOs@users.noreply.github.com>
2026-04-19 21:08:45 -0600

    added user.js

---
+++ routes/user.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const user = express.Router();
+const db = require('../config/database');
+
+user.post("/", async (req, res, next) => {
+ const { user_name, user_mail, user_password } = req.body;
+if (user_name && user_mail && user_password) { let query = "INSERT INTO user(user_name, user_mail, user_password) " ;
+query += `VALUES ('${user_name}', '${user_mail}', '${user_password}')`;
+const rows = await db.query(query);
+if(rows.affectedRows == 1) {
+    return res.status(201).json({ code: 201, message: "Usuario creado exitosamente" });
+}
+return res.status(500).json({ code: 500, message: "Error al crear el Usuario" });
+}
+
+return res.status(500).json({ code: 500, message: "Campos incompletos" });
+});
+
+module.exports = user;
